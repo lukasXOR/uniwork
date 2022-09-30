3dbde9527b3de3feab2d3531f0d5209b4396e7eb
lukas <lukas7865@yahoo.co.uk>
2022-09-30 18:57:03 +0100

    .

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App
     {
         System.out.println(ConsoleColours.ANSI_BRIGHT_BG_PURPLE);
         System.out.println(ConsoleColours.ANSI_BRIGHT_BG_YELLOW);
-        System.out.println(ConsoleColours.CONSOLE_AR);
+        System.out.println(ConsoleColours.CONSOLE_CLEAR);
 
         System.out.println("CO452 Programming Concepts 2022/23");
         System.out.println("==================================");
