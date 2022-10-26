06ae727a83b1845b07206fbc1a031147002b8c9d
lukasXOR <50531317+lukasXOR@users.noreply.github.com>
2022-10-26 02:09:06 +0100

    .

--- src/song/songmanager.java
+++ src/song/songmanager.java
@@ -9,26 +9,46 @@ class song {
         this.sPlaycount = playCount;
     }
 }
-public class songmanager {
+public class HelloWorld{
     static final String format = "|%1$-15s|%2$-15s|%3$-15s|\n";
     static ArrayList<song> songs = new ArrayList<song>();
     /*
-    
-    Ed Sheeran	Shape of You	3,746,122,511	985,618	2017
-		The Weeknd	Blinding Lights	3,288,094,770	1,715,626
-		Ed Sheeran	Perfect	3,224,789,370	1,346,254	2017
-		Luis Fonsi	Despacito	3,112,243,015	678,809	2017
-		Tones And I	Dance Monkey	2,719,548,764	678,167	
-		Lewis Capaldi	Someone You Loved	2,609,765,817	1,289,611	2018
-		Post Malone	rockstar (feat. 21 Savage)	2,569,898,717	729,633	2017
-		Post Malone	Sunflower - Spider-Man: Into the Spider-Verse	2,500,482,535	1,010,713	2018
-9		Lil Nas X	Old Town Road	2,490,171,174	639,539	2019
-10		Billie Eilish	bad guy	2,480,025,293	697,908	2019
-11
+    Ed Sheeran-Shape of You-3,746,122,511
+	The Weeknd-Blinding Lights-3,288,094,770
+	Ed Sheeran-Perfect-3,224,789,370
+	Luis Fonsi-Despacito 3,112,243,015
+	Tones And I-Dance Monkey-2,719,548,764
+	Lewis Capaldi-Someone You Loved-2,609,765,817
+	Post Malone-rockstar (feat. 21 Savage)-2,569,898,717
+	Post Malone-Sunflower Spider-Man: Into the Spider Verse-2,500,482,535
+	Lil Nas X-Old Town Road-2,490,171,174
+	Billie Eilish-bad guy-2,480,025,293
+
 */
     static {
-        songs.add(new song("Ed Sheeran", "3", 12321L));
+        String baseSongs[] = retrieveBaseSongs().split("\r?\\n");
+        for (int i = 0; i < baseSongs.length; i++) {
+            String song[] = baseSongs[i].split("-");
+            String name = song[0];
+            String artist = song[1];
+            Long playcount = Long.parseLong(song[2].replace(",",""));
+            songs.add(new song(name, artist, playcount));
+        }  
+        //songs.add(new song("Ed Sheeran", "Shapeof ", 12321L));
     }
+    public static String retrieveBaseSongs() {
+        return 
+        "Ed Sheeran-Shape of You-3,746,122,511\n" +
+        "The Weeknd-Blinding Lights-3,288,094,770\n" +
+        "Ed Sheeran-Perfect-3,224,789,370\n" +
+        "Luis Fonsi-Despacito-3,112,243,015\n" +
+        "Tones And I-Dance Monkey-2,719,548,764\n" +
+        "Lewis Capaldi-Someone You Loved-2,609,765,817\n" +
+        "Post Malone-rockstar (feat. 21 Savage)-2,569,898,717\n" +
+        "Post Malone-Sunflower SpiderMan: Into the Spider Verse-2,500,482,535\n" +
+        "Lil Nas X-Old Town Road-2,490,171,174\n" +
+	    "Billie Eilish-bad guy-2,480,025,293\n";
+    } 
      public static void main(String []args){
         display();
      }
