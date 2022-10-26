97fd2002d7a8c813478c85c771c7a273cb8960b3
lukasXOR <50531317+lukasXOR@users.noreply.github.com>
2022-10-26 02:09:51 +0100

    Update songmanager.java

--- src/song/songmanager.java
+++ src/song/songmanager.java
@@ -3,19 +3,25 @@ class song {
     String sName;
     String sArtist;
     long sPlaycount; // top songs have playcount over 'int' range
-    public song(String name, String artist, long playCount) {
+    public song(String name, String artist, long playCount) { // constructor
         this.sName = name;
-        this.sArtist = artist;
+        this.sArtist = artist;                                                        
         this.sPlaycount = playCount;
     }
 }
 class manager {
     private static final String format = "|%1$-20s|%2$-20s|%3$-15s|\n";
-    public static void add() {
-        
+    public static ArrayList<song> add(ArrayList<song> songs, String[] usrAddSong) {
+        songs.add(new song(usrAddSong[0], usrAddSong[1], Long.parseLong(usrAddSong[2])));
+        System.out.println("Song added");
+        return songs;
     }
     public static void remove() {
-        
+        manager.clear();
+        System.out.println("Remove by :")
+        System.out.println("1. Index")
+        System.out.println("2. Name")
+        System.out.println("3. Artist (removes all songs by artist name)")
     }
     public static void display(ArrayList<song> songs) {
         System.out.format(format, "Name", "Artist", "Play count");
@@ -23,10 +29,57 @@ class manager {
             System.out.format(format, s.sName, s.sArtist, s.sPlaycount);
          }
     }
+    public static void clear() { // not sure if this works with all terminals
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();   
+    }
 }
-public class HelloWorld{
+public class Main{
     static ArrayList<song> songs = new ArrayList<song>();
-
+    public static String welcomeMsg(String[] msg) {
+        return msg.length == 0 ? "Welcome to Song manager" : msg[0];
+    }
+     public static void main(String ...args) {
+         System.out.println(welcomeMsg(args));
+         System.out.println("Song manager menu\n");
+         System.out.println("1. Add song");
+         System.out.println("2. Remove song");
+         System.out.println("3. View song(s)");
+         System.out.println("4. Quit");
+         switch ((int)System.console().readLine().charAt(0)) {
+            case 49: //1
+                System.out.println("Enter the song with format being 'name-artist-playcount'");
+                String usrSong = System.console().readLine();
+                String[] usrSongInfo = usrSong.split("-");
+                if (usrSongInfo.length != 3) main("Invalid format provided");
+                try {
+                    long playcount = Long.parseLong(usrSongInfo[2], 10);
+                    songs = manager.add(songs, usrSongInfo);
+                    main();
+                } catch (Exception NumberFormatException) {
+                    main("Play count provided was not a number");
+                }
+                break;
+            case 50: //2
+                songs = manager.remove();
+                break;
+            case 51: //3
+                manager.display(songs);
+                main();
+                break;
+            case 52: //4
+                System.exit(0);
+                break;
+            default:
+                manager.clear();
+                main("Invalid option provided\nPlease try again");
+                break;
+         }
+     }
+     public static void display() {
+        manager.display(songs);
+     }
+     
     static { //Initiliase base songs 
         String baseSongs[] = retrieveBaseSongs().split("\r?\\n");
         for (int i = 0; i < baseSongs.length; i++) {
@@ -49,17 +102,5 @@ public class HelloWorld{
         "Post Malone-Sunflower SpiderMan-2,500,482,535\n" +
         "Lil Nas X-Old Town Road-2,490,171,174\n" +
 	    "Billie Eilish-bad guy-2,480,025,293\n";
-    } 
-     public static void main(String []args) throws Exception{
-         System.out.println("Song manager menu");
-         System.out.println("1. Add song");
-         System.out.println("2. Remove song");
-         System.out.println("3. View song(s)");
-         System.out.println("4. Quit");
-         String usrInput = System.console().readLine();
-         System.out.println(usrInput.charAt(0));
-     }
-     public static void display() {
-        manager.display(songs);
-     }
+    }
 }
